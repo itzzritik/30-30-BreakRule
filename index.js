fe1c81ac6251ea64537e92e9c40140fbe63d16c6
Restyled.io <commits@restyled.io>
2020-10-01 08:22:39 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const notifier = require('node-notifier'),
-	notify = () => {
-		notifier.notify({
-			title: 'Eye Break',
-			message: 'Time to rest your eyes',
-			icon: './logo.jpg'
-		});
+const notifier = require('node-notifier'), notify = () => {
+  notifier.notify({
+    title : 'Eye Break',
+    message : 'Time to rest your eyes',
+    icon : './logo.jpg'
+  });
 
-		setTimeout(function () {
-			notifier.notify({
-				title: 'Eye Break',
-				message: 'You can continue your work now',
-				icon: './logo.jpg'
-			});
-		}, 30000 );
-		setTimeout(notify, 1800000 );
-	};
+  setTimeout(function() {
+    notifier.notify({
+      title : 'Eye Break',
+      message : 'You can continue your work now',
+      icon : './logo.jpg'
+    });
+  }, 30000);
+  setTimeout(notify, 1800000);
+};
 
 notify();
