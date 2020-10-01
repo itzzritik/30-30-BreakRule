83f73024eb47337e994c8bc37ff1d948d6af986f
Restyled.io <commits@restyled.io>
2020-10-01 08:22:39 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const notifier = require('node-notifier'), notify = () => {
-  notifier.notify({
-    title : 'Eye Break',
-    message : 'Time to rest your eyes',
-    icon : './logo.jpg'
-  });
-
-  setTimeout(function() {
+const notifier = require("node-notifier"),
+  notify = () => {
     notifier.notify({
-      title : 'Eye Break',
-      message : 'You can continue your work now',
-      icon : './logo.jpg'
+      title: "Eye Break",
+      message: "Time to rest your eyes",
+      icon: "./logo.jpg",
     });
-  }, 30000);
-  setTimeout(notify, 1800000);
-};
+
+    setTimeout(function () {
+      notifier.notify({
+        title: "Eye Break",
+        message: "You can continue your work now",
+        icon: "./logo.jpg",
+      });
+    }, 30000);
+    setTimeout(notify, 1800000);
+  };
 
 notify();
